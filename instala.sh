50cc8b337ffcc5acfbdb2293cdfe34f5772b9633
theonejpas <40813121+theonejpas@users.noreply.github.com>
2020-07-09 05:23:58 -0500

    update instala.sh

--- instala.sh
+++ instala.sh
@@ -15,6 +15,7 @@ SCPfrm="/etc/ger-frm"
 SCPfrm3="/etc/adm-lite"
 SCPinst="/etc/ger-inst"
 rm $HOME/Juancitoxd.sh
+SCPresq="aHR0cHM6Ly9yYXcuZ2l0aHVidXNlcmNvbnRlbnQuY29tL3RoZW9uZWpwYXMvYWRtLWRhbmtlL21hc3Rlci9yZXF1ZXN0"
 [[ $(dpkg --get-selections|grep -w "gawk"|head -1) ]] || apt-get install gawk -y &>/dev/null
 [[ $(dpkg --get-selections|grep -w "mlocate"|head -1) ]] || apt-get install mlocate -y &>/dev/null
 rm $(pwd)/$0 &> /dev/null
