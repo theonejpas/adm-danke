94547e58ce123160c0b0f157ddd2cf17ee72bbc5
theonejpas <40813121+theonejpas@users.noreply.github.com>
2020-07-09 03:52:19 -0500

    Juancitoxd.sh

--- Dankelthaher.sh
+++ Dankelthaher.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-apt-get update -y; apt-get upgrade -y; wget https://raw.githubusercontent.com/DankelthaherManager/ADM-MANAGER-DANKELTHAHER/master/instala.sh; chmod 777 instala.sh* && ./instala.sh*
+apt-get update -y; apt-get upgrade -y; wget https://raw.githubusercontent.com/theonejpas/adm-danke/master/instala.sh; chmod 777 instala.sh* && ./instala.sh*
 
